792e917ff4e8780c75d2bad318104609a3652072
NTAWIL001 <ntawil98@gmail.com>
2018-03-11 20:19:47 -0500

    knock on wood

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+/**
+ * @author ntawil001 and bkalsow001 and rstrelow001
+ *
+ */
+public class SimplePrint {
+
+	private String pName;
+	private int pAge;
+	/**
+	 * @param pName
+	 * @param pAge
+	 */
+	public SimplePrint(String pName, int pAge) {
+		super();
+		this.pName = pName;
+		this.pAge = pAge;
+	}
+
+	/**
+	 * @param pName the pName to set
+	 */
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
+	}
+
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tawil, Kalsow, Strelow");
+		for(int i=0; i<args.length;i++)
+		{
+			System.out.println("args[ " + i + " ] = " + args[i] );
+		}
+	}
+
+}
